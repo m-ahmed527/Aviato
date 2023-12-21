c2acaa4fd1b4333fb10fac6e36a65eb49166869d
m-ahmed527 <147666739+m-ahmed527@users.noreply.github.com>
2023-12-21 13:19:00 -0800

    Update 2023_10_12_161741_add_column_role_users_table.php

--- database/migrations/2023_10_12_161741_add_column_role_users_table.php
+++ database/migrations/2023_10_12_161741_add_column_role_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-           $table->dropColumn('role')->default('user')->after('emai');
+           $table->dropColumn('role')->default('user')->after('email');
                 });
     }
 };
